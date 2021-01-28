58beffeb4bd0ce94fbe1f1781aead71f1ee393df
Hristo Oviedo <hristo.oviedohn@gmail.com>
2021-01-27 21:10:53 -0600

    Se agregaron las rutas para la salidas html y json

--- solución_segundo_proyecto_2/MyProjectApp/routes.jl
+++ solución_segundo_proyecto_2/MyProjectApp/routes.jl
@@ -1,5 +1,10 @@
 using Genie.Router
+using BooksController
 
 route("/") do
   serve_static_file("welcome.html")
 end
+
+route("/mfbooks", BooksController.myfavoritebooks)
+
+route("/api/v1/mfbooks", BooksController.API.myfavoritebooks)
