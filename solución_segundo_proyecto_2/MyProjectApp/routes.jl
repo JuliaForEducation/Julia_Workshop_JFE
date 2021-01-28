1d890d053287eeaab0babc5a384a47132f5e1bc7
Hristo Oviedo <hristo.oviedohn@gmail.com>
2021-01-27 23:59:41 -0600

    Modificaciones estéticas de la pantalla principal

--- solución_segundo_proyecto_2/MyProjectApp/routes.jl
+++ solución_segundo_proyecto_2/MyProjectApp/routes.jl
@@ -1,8 +1,13 @@
 using Genie.Router
+using Genie.Renderer.Html, Genie.Requests
 using BooksController
 
 route("/") do
   serve_static_file("welcome.html")
+end
+
+route("/name", method = POST) do
+    "Felicidades $(postpayload(:name, "joven anónimo"))"
 end
 
 route("/mfbooks", BooksController.myfavoritebooks)
